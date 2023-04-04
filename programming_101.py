dbc9bcb3a46a8712ff019a617b96ad8c749632fe
Jason Ho <hokinlokj@gmail.com>
2023-04-04 23:28:09 +0800

    Add `programming_101`

---
+++ programming_101.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+base = 6
+height = 9
+area = base * height / 2
+result = "The area of the triangle is " + str(area) + " cm^2"
+print(result)
+# The area of the triangle is 27 cm^2
+
+
+side_1 = 7
+side_2 = 7
+side_3 = 7
+if side_1 == side_2 and side_2 == side_3:
+  print("It is an equilateral triangle!")
+elif side_1 == side_2 or side_1 == side_3 or side_2 == side_3:
+  print("It is an isosceles triangle!")
+else:
+  print("It is a triangle.")
+# It is an equilateral triangle!
+
+
+adjectives = ["handsome", "smart", "kind", "tall"]
+for word in adjectives:
+  sentence = "I am very " + word + "."
+  print(sentence)
+
+
+print("Multiples of 3:")
+for index in range(10):
+  multiple = 3 * (index + 1)
+  print(str(index + 1) + ": " + str(multiple))
