0393bd9cdb5e92373b15d2499ac3687cfbcfbc90
Jason Ho <hokinlokj@gmail.com>
2023-04-05 23:51:18 +0800

    Add answer for `programming_101`

--- programming_101.py
+++ programming_101.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-base = 6
+upper_base = 6
+lower_base = 8
 height = 9
-area = base * height / 2
-result = "The area of the triangle is " + str(area) + " cm^2"
+area = (upper_base + lower_base) * height / 2
+result = "The area of the trapezium is " + str(area) + " cm^2"
 print(result)
-# The area of the triangle is 27 cm^2
+# The area of the trapezium is 63.0 cm^2
 
 
 side_1 = 7
 side_2 = 7
 side_3 = 7
-if side_1 == side_2 and side_2 == side_3:
-  print("It is an equilateral triangle!")
-elif side_1 == side_2 or side_1 == side_3 or side_2 == side_3:
-  print("It is an isosceles triangle!")
+side_4 = 7
+if side_1 == side_2 and side_2 == side_3 and side_3 == side_4:
+  print("It is a square!")
+elif side_1 == side_3 and side_2 == side_4:
+  print("It is a rectangle!")
 else:
-  print("It is a triangle.")
-# It is an equilateral triangle!
+  print("It is a quadrilateral.")
+# It is a square!
